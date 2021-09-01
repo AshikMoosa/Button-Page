1e670d9bcab324a2f91dd340deff747d7ae0e709
MysticMoz <ashikmoosa96@gmail.com>
2021-09-01 23:20:01 +0530

    first commit

---
+++ src/component/InputPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function InputPage() {
+  return (
+    <div className="input-section">
+      <div className="container">
+        <h1>Inputs</h1>
+        <h3>Coming Soon...</h3>
+      </div>
+    </div>
+  );
+}
+
+export default InputPage;
